78a2c45bcf2a0ef1b8157cc86d81a08d9f4bf6b0
Kamas 'Iceberg' Lau <kamaslau@users.noreply.github.com>
2024-10-01 20:55:38 +0800

    deps: update dependencies

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"github.com/joho/godotenv"
 )
 
+var port = "3000"
+
 func loadEnv() {
 	err := godotenv.Load(".env")
 	if err != nil {
@@ -18,13 +20,8 @@ func loadEnv() {
 }
 
 func main() {
-	// Load env variable(s)
-	loadEnv()
-	port := "3000"
+	loadEnv() // Load env variable(s)
 	fmt.Printf("env.PORT: %s", os.Getenv("PORT"))
-	if strings.Count(os.Getenv("PORT"), "") > 0 {
-		port = os.Getenv("PORT")
-	}
 
 	app := fiber.New()
 
@@ -37,6 +34,9 @@ func main() {
 		return c.JSON(response)
 	})
 
+	if strings.Count(os.Getenv("PORT"), "") > 0 {
+		port = os.Getenv("PORT")
+	}
 	err := app.Listen(fmt.Sprintf(":%s", port))
 	if err != nil {
 		fmt.Println("Error trying to launching fiber: ", err)
