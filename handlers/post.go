5def3a08a4f2149f1087a632bf3db4963f90c9e8
Kamas 'Iceberg' Lau <kamaslau@users.noreply.github.com>
2024-10-05 23:31:40 +0800

    feat: update handler for post route

--- handlers/post.go
+++ handlers/post.go
@@ -11,6 +11,8 @@ func Find(c fiber.Ctx) error {
 	fmt.Println("Find: ")
 
 	var data []models.Post
+
+	// Do
 	models.DBClient.Find(&data)
 	if len(data) == 0 {
 		return c.Status(404).JSON(ResNotFound)
@@ -23,7 +25,7 @@ func Find(c fiber.Ctx) error {
 
 func FindOne(c fiber.Ctx) error {
 	var id = c.Params("id")
-	fmt.Printf("FindOne: id=%s", id)
+	fmt.Printf("FindOne: id=%s\n", id)
 
 	var data models.Post
 	conditions := map[string]interface{}{"ID": id}
@@ -40,14 +42,18 @@ func FindOne(c fiber.Ctx) error {
 func Create(c fiber.Ctx) error {
 	fmt.Println("Create: ")
 
-	payload := new(models.Post)
-	err := c.Bind().Body(payload)
+	// Parse payload
+	var payload models.Post
+	err := c.Bind().Body(&payload)
 	if err != nil {
 		fmt.Println(err)
 		return c.Status(400).JSON(fiber.Map{"succeed": "no", "message": "input error"})
+	} else {
+		fmt.Printf("payload: %#v\n", &payload)
 	}
 	payload.UUID = uuid.NewString()
 
+	// Do
 	result := models.DBClient.Create(&payload)
 	if result.RowsAffected == 1 {
 		response := fiber.Map{"succeed": "yes", "id": payload.ID}
@@ -58,23 +64,39 @@ func Create(c fiber.Ctx) error {
 	}
 }
 
-// TODO
 func Update(c fiber.Ctx) error {
 	var id = c.Params("id")
-	fmt.Printf("Update: id=%s", id)
+	fmt.Printf("Update: id=%s\n", id)
 
 	conditions := map[string]interface{}{"ID": id}
 
-	// Lookup Target(s)
-	var data []models.Post
-	models.DBClient.Where(conditions).Find(&data) // No need to add 'deleted_at is null', GORM adds it by default with gorm.Model from type
-	if len(data) == 0 {
+	// Lookup Target
+	var data models.Post
+	models.DBClient.Where(conditions).First(&data)
+	if data.ID == 0 {
 		return c.Status(404).JSON(ResNotFound)
+	} else {
+		fmt.Printf("target: %#v\n", &data)
 	}
 
-	// TODO Do
-	var payload = models.Post{}
-	result := models.DBClient.Save(&payload)
+	// Parse payload
+	var payload models.Post
+	err := c.Bind().Body(&payload)
+	if err != nil {
+		fmt.Println(err)
+		return c.Status(400).JSON(fiber.Map{"succeed": "no", "message": "input error"})
+	} else {
+		fmt.Printf("payload: %#v\n", &payload)
+	}
+
+	// Merge payload to current data
+	// TODO Optimize to map fields automatically
+	data.Name = payload.Name
+	data.Content = payload.Content
+	data.Excerpt = payload.Excerpt
+
+	// Do
+	result := models.DBClient.Save(&data)
 	if result.RowsAffected != 1 {
 		fmt.Println(result.Error)
 		return c.Status(500).JSON(fiber.Map{"succeed": "no", "message": "Failed to update"})
@@ -85,7 +107,7 @@ func Update(c fiber.Ctx) error {
 
 func Delete(c fiber.Ctx) error {
 	var id = c.Params("id")
-	fmt.Printf("Delete: id=%s", id)
+	fmt.Printf("Delete: id=%s\n", id)
 
 	conditions := map[string]interface{}{"ID": id}
 
