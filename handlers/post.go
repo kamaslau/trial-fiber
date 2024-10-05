50ffe4c922960440f0f9fea8cd87a3a756c94d44
Kamas 'Iceberg' Lau <kamaslau@users.noreply.github.com>
2024-10-05 21:57:34 +0800

    refactor: accept passed-in body in post create handler

--- handlers/post.go
+++ handlers/post.go
@@ -40,11 +40,14 @@ func FindOne(c fiber.Ctx) error {
 func Create(c fiber.Ctx) error {
 	fmt.Println("Create: ")
 
-	var payload = models.Post{
-		UUID:    uuid.NewString(),
-		Name:    "DB connected",
-		Content: "This is an auto generated message on database connection succeed.",
+	payload := new(models.Post)
+	err := c.Bind().Body(payload)
+	if err != nil {
+		fmt.Println(err)
+		return c.Status(400).JSON(fiber.Map{"succeed": "no", "message": "input error"})
 	}
+	payload.UUID = uuid.NewString()
+
 	result := models.DBClient.Create(&payload)
 	if result.RowsAffected == 1 {
 		response := fiber.Map{"succeed": "yes", "id": payload.ID}
@@ -57,10 +60,27 @@ func Create(c fiber.Ctx) error {
 
 // TODO
 func Update(c fiber.Ctx) error {
-	fmt.Println("Update: ")
+	var id = c.Params("id")
+	fmt.Printf("Update: id=%s", id)
 
-	response := fiber.Map{"title": "Update", "content": "This would be an update op."}
-	return c.JSON(response)
+	conditions := map[string]interface{}{"ID": id}
+
+	// Lookup Target(s)
+	var data []models.Post
+	models.DBClient.Where(conditions).Find(&data) // No need to add 'deleted_at is null', GORM adds it by default with gorm.Model from type
+	if len(data) == 0 {
+		return c.Status(404).JSON(ResNotFound)
+	}
+
+	// TODO Do
+	var payload = models.Post{}
+	result := models.DBClient.Save(&payload)
+	if result.RowsAffected != 1 {
+		fmt.Println(result.Error)
+		return c.Status(500).JSON(fiber.Map{"succeed": "no", "message": "Failed to update"})
+	}
+
+	return c.JSON(fiber.Map{"succeed": "yes"})
 }
 
 func Delete(c fiber.Ctx) error {
@@ -80,7 +100,7 @@ func Delete(c fiber.Ctx) error {
 	result := models.DBClient.Where(conditions).Delete(&models.Post{})
 	if result.RowsAffected != 1 {
 		fmt.Println(result.Error)
-		return c.Status(500).JSON(fiber.Map{"succeed": "no", "message": "Failed to delete note"})
+		return c.Status(500).JSON(fiber.Map{"succeed": "no", "message": "Failed to delete"})
 	}
 
 	return c.JSON(fiber.Map{"succeed": "yes"})
