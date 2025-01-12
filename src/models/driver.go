38dc27b3924dd9e69759827afd365f6314c16276
Kamas 'Iceberg' Lau <kamaslau@users.noreply.github.com>
2025-01-13 04:24:03 +0800

    refactor: improve database driver

--- src/models/driver.go
+++ src/models/driver.go
@@ -1,7 +1,7 @@
 package models
 
 import (
-	"fmt"
+	"log"
 	"os"
 	"strconv"
 
@@ -15,36 +15,39 @@ var DBClient *gorm.DB
 
 // ConnectDB 建立连接
 func ConnectDB() {
-	dsn := os.Getenv("DSN")
+	DB_DSN := os.Getenv("DB_DSN")
+	// log.Print("DB_DSN: ", DB_DSN)
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(DB_DSN), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database: " + err.Error())
+	} else {
+		DBClient = db
+		log.Print("DB connected")
 	}
-	fmt.Println("DB connected")
-	DBClient = db
 
 	//https://gorm.io/docs/migration.html#Auto-Migration
 	err = DBClient.AutoMigrate(&Post{})
 	if err != nil {
 		panic("failed to migrate database: " + err.Error())
+	} else {
+		logOnConnected()
 	}
-
-	logOnConnected()
 }
 
-// logOnConnected 写日志：数据库连接成功时
+// logOnConnected 写日志：数据库连接成功
 func logOnConnected() {
 	var payload = Post{
 		UUID:    uuid.NewString(),
 		Name:    "DB connected",
 		Content: "This is an auto generated message on database connection succeed.",
 	}
+
 	result := DBClient.Create(&payload)
 	if result.RowsAffected == 1 {
-		fmt.Println("succeed to insert data: id:" + strconv.Itoa(int(payload.ID)))
+		log.Print("└ Succeed inserting data ID:" + strconv.Itoa(int(payload.ID)))
 	} else {
-		fmt.Println(result.Error)
+		log.Print(result.Error)
 		panic("failed to insert data")
 	}
 }
