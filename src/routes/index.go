28e7d63f7ec72a236725fdba35a52ac95d13f8c8
Kamas 'Iceberg' Lau <kamaslau@users.noreply.github.com>
2025-01-18 05:06:28 +0800

    feat: append /health route for health monitoring

--- src/routes/index.go
+++ src/routes/index.go
@@ -6,12 +6,20 @@ import (
 	"github.com/gofiber/fiber/v3"
 )
 
+// healthCheck For monitoring
+func healthCheck(c fiber.Ctx) error {
+	return c.JSON(fiber.Map{
+		"status": "ok",
+	})
+}
+
 func Root(c fiber.Ctx) error {
 	return c.SendString("Hello, World!")
 }
 
 func InitRoutes(app *fiber.App) {
 	app.Get("/", Root)
+	app.Get("/health", healthCheck)
 
 	// RESTful
 	post.InitRoutes(app)
